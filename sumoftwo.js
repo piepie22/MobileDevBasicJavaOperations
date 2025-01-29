8af834efb84330e80d9dc43ff54dab418879b5ea
piepie22 <106939195+piepie22@users.noreply.github.com>
2025-01-28 23:20:10 -0500

    Create sumoftwo.js

---
+++ sumoftwo.js
@@ -0,0 +1,6 @@
+function sumTwoNumbers(num1, num2) { //function sumTwoNumbers 
+  return num1 + num2; //adding 2 numbers
+}
+const result = sumTwoNumbers(2, 5); // implemented argument factors
+//console log
+console.log("Sum of two numbers: 2 + 5 =", result); 
